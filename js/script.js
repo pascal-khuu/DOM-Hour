90dd22e9076922af022c1b815bf4f58cf3daf54a
Pascal Khuu <pascal.khuu@orange.fr>
2022-02-05 10:36:11 +0100

    hour red and bold when the second is multiple of 3

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
 function realtime() {
-    const d = new Date();
-    let text = d.toLocaleTimeString();
-    document.getElementById("time").innerHTML = text;
+
     setInterval(() => {
         const d = new Date();
-        let text = d.toLocaleTimeString();
-        document.getElementById("time").innerHTML = text;
+        let text = d.toLocaleTimeString("fr");
+        let hour = d.toLocaleTimeString().substring(0, 2);
+        let minute = d.toLocaleTimeString().substring(3, 5);
+        let second = d.toLocaleTimeString().substring(6, 8);
+        document.getElementById("time").innerHTML = ` ${hour} : ${minute} : ${second} `;
+        if (d.getSeconds() % 3 == 0) {
+            document.getElementById("time").innerHTML = `<span style="color:red; font-family:bold"> ${hour} </span>: ${minute} : ${second} `;
+        } else {
+            document.getElementById("time").style.color = "black";
+        }
     }, 1)
 }
+
+
 
 realtime();
